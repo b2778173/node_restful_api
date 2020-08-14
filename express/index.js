c6fb29b3863951447929064fbff2ddf8e52fdf1f
Kevin Tsai <b2778173@hotmail.com>
2020-08-14 22:13:18 +0800

    built server with express

--- express/index.js
+++ express/index.js
@@ -1,11 +1,13 @@
 const colors = require('colors');
 const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.listen(8080, function() {
-    console.log(colors.blue('listing at part 8080...'));
+app.listen(5000, function() {
+    console.log('\x1b[42m', '\x1b[30msuccess \x1b[0m', 'Server has started.');
+    console.log(colors.blue('listening at part 5000...'));
 });
