d9a11346c9102b1e0b02c1cc5862dc21af6dc573
Kevin Tsai <b2778173@hotmail.com>
2020-08-17 01:28:13 +0800

    db connection string

--- express/db/index.js
+++ express/db/index.js
@@ -3,7 +3,7 @@ const colors = require('colors');
 
 const mongoDB = () => {
     mongoose
-        .connect('mongodb://localhost/playground', { useNewUrlParser: true, useUnifiedTopology: true })
+        .connect(`mongodb://testapidb:XDKcnIL9dBEUVPecyyCeTPvdrtsuPMK1PFXQTe6iQx9FaoPzFqAgrWPUl9umCYfU3XStuTGXXBlY1oQyIFy7Mg%3D%3D@testapidb.mongo.cosmos.azure.com:10255/?ssl=true&appName=@testapidb@`, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log(colors.green('connected to DB successfully...')))
         .catch((err) => console.log(colors.red('DB connnected fail...', err)));
 }
