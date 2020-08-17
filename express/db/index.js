f5ca0bb140617b73f318aad2baaebd28b283ebe0
Kevin Tsai <b2778173@hotmail.com>
2020-08-17 13:56:05 +0800

    add env variable

--- express/db/index.js
+++ express/db/index.js
@@ -3,9 +3,12 @@ const colors = require('colors');
 
 const mongoDB = () => {
     mongoose
-        .connect(`mongodb://testapidb:XDKcnIL9dBEUVPecyyCeTPvdrtsuPMK1PFXQTe6iQx9FaoPzFqAgrWPUl9umCYfU3XStuTGXXBlY1oQyIFy7Mg%3D%3D@testapidb.mongo.cosmos.azure.com:10255/?ssl=true&appName=@testapidb@`, { useNewUrlParser: true, useUnifiedTopology: true })
+        .connect(process.env.COSMOS_DB_CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         .then(() => console.log(colors.green('connected to DB successfully...')))
         .catch((err) => console.log(colors.red('DB connnected fail...', err)));
-}
+};
 
-module.exports = mongoDB
+module.exports = mongoDB;
--- express/index.js
+++ express/index.js
@@ -1,10 +1,11 @@
 const colors = require('colors');
 const app = require('./route');
 const mongoDB = require('./db');
+require('dotenv').config();
 
-
-app.listen(5000, function() {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT || 5000, function () {
     console.log('\x1b[42m', '\x1b[30msuccess \x1b[0m', 'Server has started.');
-    console.log(colors.blue('listening at part 5000...'));
+    console.log(colors.blue(`listening at part ${PORT}...`));
 });
 mongoDB();
